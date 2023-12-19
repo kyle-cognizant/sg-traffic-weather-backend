32bcd9941d55d241bcc680d2e71783feaa1c340b
Kyle Oliveiro <kyle.oliveiro@cognizant.com>
2023-12-19 11:49:52 +0800

    feat: add type defs

---
+++ src/types.d.ts
@@ -0,0 +1,60 @@
+export type Coordinates = {
+  latitude: number
+  longitude: number
+}
+
+export type Camera = {
+  timestamp: string
+  image: string
+  location: {
+    latitude: number
+    longitude: number
+  }
+  camera_id: string
+  image_metadata: {
+    height: number
+    width: number
+    md5: string
+  }
+}
+
+export type CameraWithAreaName = Camera & {
+  area_name: string
+}
+
+export type CameraDetails {
+  camera: CameraWithAreaName
+  weather_forecast: string
+}
+
+export type GovTrafficCamApiData = {
+  items: {
+    timestamp: string
+    cameras: Camera[]
+  }[]
+  api_info: {
+    status: string
+  }
+}
+
+export type GovWeatherApiData = {
+  area_metadata: {
+    name: string
+    label_location: Coordinates
+  }[]
+  items: {
+    update_timestamp: string
+    timestamp: string
+    valid_period: {
+      start: string
+      end: string
+    }
+    forecasts: {
+      area: string
+      forecast: string
+    }[]
+  }[]
+  api_info: {
+    status: string
+  }
+}
