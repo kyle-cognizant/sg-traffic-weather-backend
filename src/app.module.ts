34c7601a04ba23d0c36f90d566fc4377d605e63f
Kyle Oliveiro <kyle.oliveiro@cognizant.com>
2023-12-19 11:31:36 +0800

    feat: scaffold cameras and reports modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CamerasModule } from './cameras/cameras.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [CamerasModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
