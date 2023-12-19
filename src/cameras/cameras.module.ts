0004d0afcd37d97bc77be3761a61ad02c51f0fe3
Kyle Oliveiro <kyle.oliveiro@cognizant.com>
2023-12-19 11:58:09 +0800

    feat: add api fetcher functions to cameras service

--- src/cameras/cameras.module.ts
+++ src/cameras/cameras.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CamerasService } from './cameras.service';
 import { CamerasController } from './cameras.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   providers: [CamerasService],
   controllers: [CamerasController]
 })
--- src/cameras/cameras.service.ts
+++ src/cameras/cameras.service.ts
@@ -1,4 +1,18 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { GovTrafficCamApiData, GovWeatherApiData } from 'src/types';
 
 @Injectable()
-export class CamerasService {}
+export class CamerasService {
+  constructor(private readonly httpService: HttpService) { }
+
+  private async fetchGovTrafficCamApiData() {
+    const { data } = await this.httpService.axiosRef.get<GovTrafficCamApiData>('https://api.data.gov.sg/v1/transport/traffic-images')
+    return data
+  }
+
+  private async fetchGovWeatherApiData(): Promise<GovWeatherApiData> {
+    const { data } = await this.httpService.axiosRef.get<GovWeatherApiData>('https://api.data.gov.sg/v1/environment/2-hour-weather-forecast')
+    return data
+  }
+}
